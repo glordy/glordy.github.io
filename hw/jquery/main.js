5d334724858f30c5b0f91bfc4c5051d86b37d0ff
glordy <gxl460@miami.edu>
2020-04-17 22:31:43 -0400

    new

--- hw/jquery/main.js
+++ hw/jquery/main.js
@@ -2,15 +2,19 @@ $(document).ready(function(){
   //all js goes in here
   console.log("we loaded");
 
-  $("h2").mouseover(function(){
-    $("h2").css("font-size","40px");
+  $("#h2").mouseover(function(){
+    $("#h2").css("font-size","40px");
   });
-  $("h2").mouseleave(function(){
-    $("h2").css("background-color","grey");
+  $("#h2").mouseleave(function(){
+    $("#h2").css("background-color","grey");
   });
   $("#button1").click(function(){
     $("#ingredients").show();
-  };
+  });
   $("#button2").click(function(){
     $("#ingredients").hide();
-  };
+  });
+$("#list").mouseover(function(){
+ $("#list").html("What You'll Need");
+});
+}
