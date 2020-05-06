038d4987b8328c80ea6eba8f1191a9664ed6a316
glordy <gxl460@miami.edu>
2020-05-06 11:19:25 -0400

    Update main.js

--- hw/final/main.js
+++ hw/final/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   //all js goes in here
   console.log("we loaded");
-  $("#h3").mouseover(function(){
-    $("#h3").css("font-size","40px");
+  $("h3").mouseover(function(){
+    $("h3").css("font-size","40px");
   });
-  $("#p").mouseover(function(){
-    $("#p").css("background-color","grey");
-  });
+  $("p").mouseover(function(){
+    $("p").css("background-color","grey");
+  }));
