8797e340c380eff9f5b5c82951f14b2b40a497c7
glordy <gxl460@miami.edu>
2020-05-06 11:03:26 -0400

    new

---
+++ hw/final/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  //all js goes in here
+  console.log("we loaded");
+  $("#h3").mouseover(function(){
+    $("#h3").css("font-size","40px");
+  });
+  $("#p").mouseover(function(){
+    $("#p").css("background-color","grey");
+  });
