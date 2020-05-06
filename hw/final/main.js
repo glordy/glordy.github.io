114adc1a42b6da416a9a2011c74c688eb6482c5d
glordy <gxl460@miami.edu>
2020-05-06 11:53:53 -0400

    Update main.js

--- hw/final/main.js
+++ hw/final/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
   //all js goes in here
   console.log("we loaded");
-  $("h3").mouseover(function(){
-    $("h3").css("font-size","40px");
+
+  $("#1").mouseover(function(){
+    $("#1").css("font-size","40px");
   });
-  $("p").mouseover(function(){
-    $("p").css("background-color","grey");
-  }));
+  $("#2").mouseover(function(){
+    $("#2").css("font-size","40px");
+  });
+  $("#3").mouseover(function(){
+    $("#3").css("font-size","40px");
+  });
+  $("#4").mouseover(function(){
+    $("#4").css("font-size","40px");
+  });
+  $("#p1").mouseover(function(){
+    $("#p1").css("background-color","grey");
+  });
+  $("#p2").mouseover(function(){
+    $("#p2").css("background-color","grey");
+  });
+  $("#p3").mouseover(function(){
+    $("#p3").css("background-color","grey");
+  });
+  $("#p4").mouseover(function(){
+    $("#p4").css("background-color","grey");
+  });
+});
